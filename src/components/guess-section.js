e0d93c6d687326b421b2aa82bb431cbf438e169c
ericpcho <ericpcho@gmail.com>
2017-10-05 08:38:54 -0700

    changed guess-section.js to a stateful component

--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -4,12 +4,17 @@ import GuessForm from './guess-form';
 
 import './guess-section.css';
 
-export default function GuessSection(props) {
-    return (
-        <section>
-            <h2 id="feedback">{props.feedback}</h2>
-            <GuessForm />
-        </section>
-    );
-}
+export default class GuessSection extends React.Component() {
+    constructor(props) {
+        super(props)
+    }
 
+    render(){
+        return (
+            <section>
+                <h2 id="feedback">{props.feedback}</h2>
+                <GuessForm onSubmit={this}/>
+            </section>
+        );
+    }
+}
